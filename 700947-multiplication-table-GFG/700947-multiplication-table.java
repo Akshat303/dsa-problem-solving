3db1076738fc6ea21b63a8aac6026509dcba44b2
Akshat srivastava <48360544+Akshat303@users.noreply.github.com>
2026-09-17 12:37:09 +0530

    Added solution - Time: O(1), Space: O(1)

---
+++ 700947-multiplication-table-GFG/700947-multiplication-table.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        // code here
+        for ( int i=1; i <= 10; i++){
+            int m =  n*i ;
+            System.out.print(m + " ");
+        }
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
