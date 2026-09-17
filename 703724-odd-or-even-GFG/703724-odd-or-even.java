3af6e9dc4b360dd27a74be91473f85217d7969d2
Akshat srivastava <48360544+Akshat303@users.noreply.github.com>
2026-09-17 15:07:25 +0530

    Added solution - Time: O(1), Space: O(1)

---
+++ 703724-odd-or-even-GFG/703724-odd-or-even.java
@@ -0,0 +1,10 @@
+class Solution {
+    static boolean isEven(int n) {
+        // Agar n ko 2 se divide karne par remainder 0 bachta hai, toh true (Even)
+        return (n % 2 == 0);
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
