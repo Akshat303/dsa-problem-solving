7b308f5c1b3835393398080a58b49cb9f83e3dec
Akshat srivastava <48360544+Akshat303@users.noreply.github.com>
2026-09-21 01:16:12 +0530

    ✅ LJAAS60 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-20

---
+++ codechef/medium/LJAAS60/solution.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+		// your code goes here
+        Scanner sc = new Scanner(System.in);
+        
+        for (int i = 0; i<3; i++){
+            if (sc.hasNextInt()){
+                int marks = sc.nextInt();
+                
+                if (marks > 90){
+                    System.out.println("A");
+                } else if (marks > 70){
+                    System.out.println("B");
+                }  else if (marks >= 40){
+                    System.out.println("C");
+                } else {
+                    System.out.println("F");
+                }
+            }
+        }
+        sc.close();
+	}
+}
