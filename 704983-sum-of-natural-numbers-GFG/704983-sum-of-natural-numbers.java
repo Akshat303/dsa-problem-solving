2f3f81976291a3a45eb1a1f5a721328456cda54c
Akshat srivastava <48360544+Akshat303@users.noreply.github.com>
2026-09-20 12:28:14 +0530

    Added solution - Time: O(n), Space: O(1)

---
+++ 704983-sum-of-natural-numbers-GFG/704983-sum-of-natural-numbers.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+
+        System.out.println(sum);
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
