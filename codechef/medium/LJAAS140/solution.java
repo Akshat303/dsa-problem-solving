c8885041f1bd7bd196020d0d6a4489603ae4099b
Akshat srivastava <48360544+Akshat303@users.noreply.github.com>
2026-09-21 01:49:44 +0530

    ✅ LJAAS140 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-20

---
+++ codechef/medium/LJAAS140/solution.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Take length and width as input
+        Scanner sc = new Scanner(System.in);
+        int length = sc.nextInt();
+        int width = sc.nextInt();
+        
+        int area = calculateArea(length, width);
+        System.out.println(area);  
+        
+        sc.close();
+    }
+    
+    public static int calculateArea(int length, int width) {
+        // Complete this method
+        return length * width ;
+        
+    }
+}
